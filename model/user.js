61421e68805ccaae01c60862db2747d17c9e7c8a
tosin-webdev <oladejit5@gmail.com>
2022-08-10 11:35:45 +0100

    added comment schema

--- model/user.js
+++ model/user.js
@@ -68,6 +68,6 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
