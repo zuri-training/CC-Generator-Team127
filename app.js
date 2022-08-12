98ec7db3ce693652e405f1e9517f040b6cc8aca3
tosin-webdev <oladejit5@gmail.com>
2022-08-12 10:54:55 +0100

    updated files

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ userRoute.post('/sendEmail', userRoute);
 // assign a port
 port = process.env.PORT || 7000;
 
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
